a579e420822f74a2b793d8295ceae4dc330512d1
Mabel Zigah <mabrite39@gmail.com>
2023-11-16 11:37:58 -0800

    Add

--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 # 2-print_alphabet.py
-# Zigah Mabel <mabrit39@gmail.com>
 
 """Print the alphabet in lowercase, not followed by a new line."""
 for letter in range(97, 123):
--- 0x02-python-import_modules/102-magic_calculation.py
+++ 0x02-python-import_modules/102-magic_calculation.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
-# 102-magic_calculation.py
 
 
 def magic_calculation(a, b):
